fb2deac155803b623d3704c09ae382a4934bf382
Richard Dingwall <rdingwall@gmail.com>
2009-12-27 16:31:29 +1300

    Marked and styled up book.html page. Added some beta (pretty crap actually) twitter-like dropdown menus, and switched layout from header/footer to left/right to close the gap up the top of the page.

---
+++ web/src/OneHundredBooks.Content/js/100books.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function() {
+
+    // Set up menu show/hide business
+    $("div.menu-container > button").click(function() {
+        var container = $(this).parents("div.menu-container");
+        var menu = $(this).siblings("ul.menu");
+
+        if (menu.hasClass("visible")) {
+            menu.removeClass("visible");
+        }
+        else {
+            menu.addClass("visible");
+
+            function waitForClickOutside(container) {
+                $("body").one("mousedown", function(e) {
+                    var c = container;
+                    if ($(e.target).is("div.menu-container *")) {
+                        // Click was inside the menu. Ignore.
+                        waitForClickOutside(c);
+                        return;
+                    }
+
+                    c.children("ul.menu").removeClass("visible");
+                });
+            };
+            
+            waitForClickOutside(container);
+        }
+    });
+    
+});
