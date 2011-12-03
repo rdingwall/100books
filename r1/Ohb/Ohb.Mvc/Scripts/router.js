19f10f1f9d79f09635eb787ca2f06f72bfbf5b46
Richard Dingwall <rdingwall@gmail.com>
2011-12-03 21:51:17 +0000

    Missed one

--- r1/Ohb/Ohb.Mvc/Scripts/router.js
+++ r1/Ohb/Ohb.Mvc/Scripts/router.js
@@ -1,5 +1,5 @@
 ﻿/*globals $, BackBone, searchResult, searchResultCollection,
-searchResultsListView, eventBus, define */
+ searchResultsListView, eventBus, define */
 define([
     'jquery',
     'backbone',
@@ -8,78 +8,69 @@ define([
     'eventbus',
     'views/searchresult/searchresultview',
     'lib/bootstrap/bootstrap-modal.js'
-], function (
-    $,
-    Backbone,
-    SearchResult,
-    SearchResultCollection,
-    eventBus
-) {
+], function ($, Backbone, SearchResult, SearchResultCollection, eventBus) {
     "use strict";
 
-    var instance = null;
+    var instance = null,
+        AppRouter = Backbone.Router.extend({
 
-    var AppRouter = Backbone.Router.extend({
+            routes: {
+                "search/:q": "search"
+            },
 
-        routes: {
-            "search/:q": "search"
-        },
+            initialize: function () {
+                console.log("initializing router...");
+                eventBus.bind('searchRequested', this.search);
+                eventBus.bind('searchFailed', this.onSearchFailed);
+            },
 
-        initialize: function () {
-            console.log("initializing router...");
-            eventBus.bind('searchRequested', this.search);
-            eventBus.bind('searchFailed', this.onSearchFailed);
-        },
+            search: function (q) {
+                console.log("Searching for " + q + "...");
 
-        search: function (q) {
-            console.log("Searching for " + q + "...");
+                eventBus.trigger("searchBegan", q);
 
-            eventBus.trigger("searchBegan", q);
+                $.getJSON("https://www.googleapis.com/books/v1/volumes?callback=?",
+                    {
+                        q: decodeURIComponent(q),
+                        projection: "lite"
+                    },
+                    function (json) {
+                        try {
+                            var results = new SearchResultCollection();
 
-            $.getJSON("https://www.googleapis.com/books/v1/volumes?callback=?",
-                  {
-                      q: decodeURIComponent(q),
-                      projection: "lite"
-                  },
-                  function (json) {
-                      try
-                      {
-                          var results = new SearchResultCollection();
+                            if (!json.items) {
+                                eventBus.trigger("searchReturnedNoResults");
+                                return;
+                            }
 
-                          if (!json.items)
-                          {
-                              eventBus.trigger("searchReturnedNoResults");
-                              return;
-                          }
+                            $(json.items).each(function () {
+                                results.add(SearchResult.fromGoogle($(this)[0]));
+                            });
 
-                          $(json.items).each(function () {
-                              results.add(SearchResult.fromGoogle($(this)[0]));
-                          });
+                            eventBus.trigger("searchResultsArrived", results);
+                        } catch (e) {
+                            console.log("Search error: " + e.message);
+                            eventBus.trigger("searchFailed");
+                        }
 
-                          eventBus.trigger("searchResultsArrived", results);
-                      }
-                      catch (e)
-                      {
-                          console.log("Search error: " + e.message);
-                          eventBus.trigger("searchFailed");
-                      }
-                      eventBus.trigger("searchCompleted");
-                  }).error(function(jqXHR, textStatus, errorThrown) {
-                      console.log("Search error: " + textStatus);
-                      eventBus.trigger("searchFailed");
-                  });
-        },
+                        eventBus.trigger("searchCompleted");
+                    })
+                    .error(function (jqXHR, textStatus, errorThrown) {
+                        console.log("Search error: " + textStatus);
+                        eventBus.trigger("searchFailed");
+                    });
+            },
 
-        onSearchFailed: function() {
-            console.log('showing search failed modal...');
-            $("#search-failed-modal").modal({ keyboard: true, show: true });
-        }
-    });
+            onSearchFailed: function () {
+                console.log('showing search failed modal...');
+                $("#search-failed-modal").modal({ keyboard: true, show: true });
+            }
+        });
 
-    AppRouter.getInstance = function(){
+    AppRouter.getInstance = function () {
         // summary:
         //      Gets an instance of the singleton. It is better to use
-        if(instance === null){
+        if (instance === null) {
             instance = new AppRouter();
         }
         return instance;
