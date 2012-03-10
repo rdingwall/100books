ea720af4b53d8d6a25621e57b5fbce122537cce0
Richard Dingwall <rdingwall@gmail.com>
2012-03-10 21:25:15 +0000

    Async tests running a lot smoother now.

--- r1/Ohb/Ohb.Mvc/jstests/googletests.js
+++ r1/Ohb/Ohb.Mvc/jstests/googletests.js
@@ -16,6 +16,8 @@
 
         log.info("hiya");
 
+        QUnit.config.testTimeout = 2000;
+
         module("When fetching a search result collection");
 
         asyncTest("It should perform a search and raise a resultsArrived event with the results", 4, function () {
@@ -42,34 +44,28 @@
                     equal(searchResult.get('title'), title);
                     equal(searchResult.get('authors'), authors);
                     equal(searchResult.get('smallThumbnailUrl'), smallThumbnailUrl);
+                    start();
                 })
                 .error(function () {
                     throw "failed";
+                    start();
                 });
         };
 
         asyncTest("It should include the book's subtitle", 3, function () {
             assertBookMapping('abYKXvCwEToC', "Harry Potter: the story of a global business phenomenon", "Susan Gunelius", "http://bks6.books.google.co.uk/books?id=abYKXvCwEToC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api");
-
-            setTimeout(start, 1000);
         });
 
         asyncTest("It should list multiple authors", 3, function () {
             assertBookMapping('GGpXN9SMELMC', "Head First design patterns", "Eric Freeman, Elisabeth Freeman, Kathy Sierra, Bert Bates", "http://bks9.books.google.co.uk/books?id=GGpXN9SMELMC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api");
-
-            setTimeout(start, 1000);
         });
 
         asyncTest("It should handle books without thumbnails", 3, function () {
             assertBookMapping('DAAAAAAACAAJ', "Harry Potter: 5 Years of Magic, Adventure, and Mystery at Hogwarts", "J. K. Rowling", null);
-
-            setTimeout(start, 1000);
         });
 
         asyncTest("It should handle books with no authors", 3, function () {
             assertBookMapping('3zgFSQAACAAJ', "The Girl With the Dragon Tattoo", null, "http://bks1.books.google.co.uk/books?id=3zgFSQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api");
-
-            setTimeout(start, 1000);
         });
 
 
@@ -84,15 +80,15 @@
                 wasRaised = true;
                 ok(results);
                 equal(results.length, 10);
+                start();
             });
 
             eventBus.bind('searchFailed', function (results) {
                 ok(false, "search failed!");
+                start();
             });
 
             eventBus.trigger("searchRequested", "harry potter");
-
-            setTimeout(start, 2000);
         });
 
         asyncTest('It should raise a searchBegan event', 2, function () {
@@ -102,11 +98,10 @@
             eventBus.bind('searchBegan', function (q) {
                 ok(true);
                 equal(q, "harry potter");
+                start();
             });
 
             eventBus.trigger("searchRequested", "harry potter");
-
-            setTimeout(start, 1000);
         });
 
         asyncTest('It should raise a searchCompleted event', 1, function () {
@@ -115,11 +110,10 @@
 
             eventBus.bind('searchCompleted', function (q) {
                 ok(true);
+                start();
             });
 
             eventBus.trigger("searchRequested", "harry potter");
-
-            setTimeout(start, 2000);
         });
 
         asyncTest('When the test fails, it should raise a searchFailed event', 1, function () {
@@ -128,15 +122,15 @@
 
             eventBus.bind('searchFailed', function (results) {
                 ok(true);
+                start();
             });
 
             eventBus.bind('searchResultsArrived', function (results) {
                 ok(false, "should not have been raised!");
+                start();
             });
 
             eventBus.trigger("searchRequested", "3894h9f893jhf934jf92ht8");
-
-            setTimeout(start, 2000);
         });
 
         asyncTest('When the test fails, it should raise a searchCompleted event', 1, function () {
@@ -145,15 +139,15 @@
 
             eventBus.bind('searchCompleted', function (results) {
                 ok(true);
+                start();
             });
 
             eventBus.bind('searchResultsArrived', function (results) {
                 ok(false, "should not have been raised!");
+                start();
             });
 
             eventBus.trigger("searchRequested", "3894h9f893jhf934jf92ht8");
-
-            setTimeout(start, 2000);
         });
 
         module("When requesting a search and there was no results");
@@ -164,20 +158,15 @@
 
             eventBus.bind('searchFailed', function (results) {
                 ok(false, "searchFailed was raised");
+                start();
             });
 
             eventBus.bind('searchReturnedNoResults', function (results) {
                 ok(true);
-            });
-
-            eventBus.bind('searchResultsArrived', function (results) {
-                equal(results.length, 0);
-                ok(false, "searchResultsArrived was raised");
+                start();
             });
 
             eventBus.trigger("searchRequested", "3894h9f893jhf934jf92ht8");
-
-            setTimeout(start, 2000);
         });
 
         asyncTest('It should raise a searchCompleted event', 1, function () {
@@ -186,20 +175,15 @@
 
             eventBus.bind('searchFailed', function (results) {
                 ok(false, "searchFailed was raised");
+                start();
             });
 
             eventBus.bind('searchCompleted', function (results) {
                 ok(true);
-            });
-
-            eventBus.bind('searchResultsArrived', function (results) {
-                equal(results.length, 0);
-                ok(false, "searchResultsArrived was raised");
+                start();
             });
 
             eventBus.trigger("searchRequested", "3894h9f893jhf934jf92ht8");
-
-            setTimeout(start, 2000);
         });
 
 
