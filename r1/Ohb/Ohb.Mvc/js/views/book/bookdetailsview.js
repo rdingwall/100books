289b5f57db48d1d18fb7b77794ec0c25e2e3c007
Richard Dingwall <rdingwall@gmail.com>
2012-03-12 18:09:45 +0000

    Toggling a book's status now updates the button on the page too!

--- r1/Ohb/Ohb.Mvc/js/views/book/bookdetailsview.js
+++ r1/Ohb/Ohb.Mvc/js/views/book/bookdetailsview.js
@@ -19,11 +19,25 @@ $(function () {
 
             initialize: function () {
                 eventBus.on("book:requested", this.onBookRequested, this);
+                this.setModel(this.model);
+            },
+
+            setModel: function (model) {
+                if (this.model) {
+                    // Unbind previous model's events...
+                    this.model.off("change:hasPreviouslyRead",
+                        this.onModelStatusChanged, this);
+                }
+                if (model) {
+                    this.model = model;
+                    this.model.on("change:hasPreviouslyRead",
+                        this.onModelStatusChanged, this);
+                }
             },
 
             onBookRequested: function (id) {
                 log.info("Fetching book from API...");
-                this.model = new Book({ id: id });
+                this.setModel(new Book({ id: id }));
                 this.model.fetch({
                     success: _.bind(this.render, this),
                     error: _.bind(this.onFetchError, this)
@@ -35,12 +49,31 @@ $(function () {
 
                 $.get("/templates/book/bookdetails.html", "text",
                     _.bind(function (template) {
-                        $(this.el).html(Mustache.to_html(template, this.model.toJSON()));
+                        var el = $(Mustache.to_html(template, this.model.toJSON()));
+                        this.renderStatusButtonPartial(el);
+                        $(this.el).html(el);
                         $(this.el).show();
                         eventBus.trigger("book:rendered", this.model);
                     }, this));
 
                 return this;
+            },
+
+            onModelStatusChanged: function () {
+                this.renderStatusButtonPartial($(this.el));
+            },
+
+            renderStatusButtonPartial: function (el) {
+
+                var newButton;
+                if (this.model.get("hasPreviouslyRead")) {
+                    newButton = $("<a class='status-toggle-button btn btn-success large'><i class='icon-ok icon-white'></i>You have read this book</a>");
+
+                } else {
+                    newButton = $("<a class='status-toggle-button btn large'><i class='icon-remove'></i>You have not read this book</a>");
+                }
+
+                $(el).find(".status-toggle-region").html(newButton);
             },
 
             onFetchError: function () {
