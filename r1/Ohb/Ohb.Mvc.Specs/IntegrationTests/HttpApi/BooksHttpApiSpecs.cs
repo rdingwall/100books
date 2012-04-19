770fca8b09c00643773c8fae6dafe8495b4ab60b
Richard Dingwall <rdingwall@gmail.com>
2012-04-19 21:30:07 +0100

    Fixed more failing tests due to google book info changes...

--- r1/Ohb/Ohb.Mvc.Specs/IntegrationTests/HttpApi/BooksHttpApiSpecs.cs
+++ r1/Ohb/Ohb.Mvc.Specs/IntegrationTests/HttpApi/BooksHttpApiSpecs.cs
@@ -25,7 +25,7 @@ namespace Ohb.Mvc.Specs.IntegrationTests.HttpApi
                 () => response.Data.Id.ShouldEqual("4YydO00I9JYC");
             
             It should_get_the_books_title =
-                () => response.Data.Title.ShouldEqual("The Google story");
+                () => response.Data.Title.ShouldEqual("The Google Story");
             
             It should_get_the_books_authors =
                 () => response.Data.Authors.ShouldEqual("David A. Vise, Mark Malseed");
@@ -40,7 +40,7 @@ namespace Ohb.Mvc.Specs.IntegrationTests.HttpApi
                 () => response.Data.Description.ShouldEqual("\"Here is the story behind one of the most remarkable Internet successes of our time. Based on scrupulous research and extraordinary access to Google, the book takes you inside the creation and growth of a company whose name is a favorite brand and a standard verb recognized around the world. Its stock is worth more than General Motors’ and Ford’s combined, its staff eats for free in a dining room that used to be\u003cb\u003e \u003c/b\u003erun\u003cb\u003e \u003c/b\u003eby the Grateful Dead’s former chef, and its employees traverse the firm’s colorful Silicon Valley campus on scooters and inline skates.\u003cbr\u003e\u003cbr\u003e\u003cb\u003eTHE GOOGLE STORY \u003c/b\u003eis the definitive account of the populist media company powered by the world’s most advanced technology that in a few short years has revolutionized access to information about everything for everybody everywhere. \u003cbr\u003eIn 1998, Moscow-born Sergey Brin and Midwest-born Larry Page dropped out of graduate school at Stanford University to, in their own words, “change the world” through a search engine that would organize every bit of information on the Web for free.\u003cbr\u003e\u003cbr\u003eWhile the company has done exactly that in more than one hundred languages, Google’s quest continues as it seeks to add millions of library books, television broadcasts, and more to its searchable database. \u003cbr\u003eReaders will learn about the amazing business acumen and computer wizardry that started the company on its astonishing course; the secret network of computers delivering lightning-fast search results; the unorthodox approach that has enabled it to challenge Microsoft’s dominance and shake up Wall Street. Even as it rides high, Google wrestles with difficult choices that will enable it to continue expanding while sustaining the guiding vision of its founders’ mantra: DO NO EVIL.\"");
             
             It should_get_the_books_thumbnail_url =
-                () => response.Data.ThumbnailUrl.ShouldEqual("http://bks2.books.google.co.uk/books?id=4YydO00I9JYC&printsec=frontcover&img=1&zoom=1&source=gbs_api");
+                () => response.Data.ThumbnailUrl.ShouldStartWith("http://bks2.books.google.co.uk/books?id=4YydO00I9JYC&printsec=frontcover&img=1&zoom=1");
             
             It should_not_be_previously_read =
                 () => response.Data.HasPreviouslyRead.ShouldBeFalse();
@@ -54,7 +54,7 @@ namespace Ohb.Mvc.Specs.IntegrationTests.HttpApi
             It should_return_the_google_book_link =
                 () =>
                 response.Data.GoogleBookUrl.ShouldEqual(
-                    "http://books.google.co.uk/books/about/The_Google_story.html?id=4YydO00I9JYC");
+                    "http://books.google.co.uk/books/about/The_Google_Story.html?id=4YydO00I9JYC");
 
             It should_return_the_google_preview_link =
                 () => response.Data.GooglePreviewUrl.ShouldEqual(
