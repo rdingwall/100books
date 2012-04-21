f1e4a45a02b31c750eb47e53df187264d042a294
Richard Dingwall <rdingwall@gmail.com>
2012-04-21 14:55:41 +0100

    Added new <meta name="ohb-app-version"> header with the assembly version. To make debugging deployments easier.

---
+++ r1/Ohb/Ohb.Mvc/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace Ohb.Mvc.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        static readonly string version = 
+            Assembly.GetExecutingAssembly().GetName().Version.ToString(); 
+
+        public static string AssemblyVersion(this HtmlHelper helper)
+        {
+            return version;
+        }
+
+        public static string AssemblyVersion<T>(this HtmlHelper<T> helper)
+        {
+            return version;
+        }
+    }
+}
