538b0b2ebdf019ab5c3f010f7ec0e1a1194f1df2
Richard Dingwall <rdingwall@gmail.com>
2012-04-29 16:00:51 +0100

    Added mocha logged in scripts to to teamcity build script.

--- r1/Ohb/build/phantomjs-tests.js
+++ r1/Ohb/build/phantomjs-tests.js
@@ -2,20 +2,29 @@
 console.log('Loading a web page');
 var page = new WebPage();
 //This was tricky, this is the way to open LOCAL files
-var url = "http://localhost:88/jstests/jasmine.html";
+
+
+var urls = [ 
+    "http://localhost:88/jstests/jasmine.html",
+    "http://localhost:88/jstests/mocha-loggedin.html"
+    ];
+
 phantom.viewportSize = {width: 800, height: 600};
 //This is required because PhantomJS sandboxes the website and it does not show up the console messages form that page by default
 page.onConsoleMessage = function (msg) { console.log(msg); };
 //Open the website
-page.open(url, function (status) {
-    //Page is loaded!
-    if (status !== 'success') {
-        console.log('Unable to load the address!');
-    } else {
-        //Using a delay to make sure the JavaScript is executed in the browser
-        window.setTimeout(function () {
-            page.render("output.png");
-            phantom.exit();
-        }, 5000);
-    }
-});
+
+for (url in urls) {
+    page.open(url, function (status) {
+        //Page is loaded!
+        if (status !== 'success') {
+            console.log('Unable to load the address!');
+        } else {
+            //Using a delay to make sure the JavaScript is executed in the browser
+            window.setTimeout(function () {
+                page.render("output.png");
+                phantom.exit();
+            }, 5000);
+        }
+    });
+}
