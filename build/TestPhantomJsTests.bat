943bcba568fa9197ff0b3c3c061d7e22e4bb2182
Richard Dingwall <rdingwall@gmail.com>
2012-06-17 16:32:07 +0100

    Now running mocha and jasmine tests in separate PhantomJS sessions (simpler).

---
+++ build/TestPhantomJsTests.bat
@@ -0,0 +1,4 @@
+@echo off
+C:\phantomjs-1.5.0-win32-static\phantomjs.exe phantomjs-tests.js http://localhost:88/jstests/jasmine.html
+C:\phantomjs-1.5.0-win32-static\phantomjs.exe phantomjs-tests.js http://localhost:88/jstests/mocha-loggedin.html
+pause
--- build/phantomjs-tests.js
+++ build/phantomjs-tests.js
@@ -1,37 +1,29 @@
-// from http://blog.danmerino.com/continuos-integration-ci-for-javascript-jasmine-and-teamcity/
-console.log('Loading a web page');
+(function () {
+    "use strict";
+    var system = require("system");
 
-//This was tricky, this is the way to open LOCAL files
+    var url = system.args[1];
 
-
-var urls = [
-    "http://localhost:88/jstests/jasmine.html",
-    "http://localhost:88/jstests/mocha-loggedin.html"
-];
-
-phantom.viewportSize = {width: 800, height: 600};
-
-for (var i in urls) {
-
-    var url = urls[i];
+    phantom.viewportSize = {width: 800, height: 600};
 
     console.log("Opening " + url);
 
     var page = new WebPage();
     //This is required because PhantomJS sandboxes the website and it does not show up the console messages form that page by default
     page.onConsoleMessage = function (msg) { console.log(msg); };
-    //Open the website
 
+    //Open the website
     page.open(url, function (status) {
         //Page is loaded!
         if (status !== 'success') {
             console.log('Unable to load the address!');
+            phantom.exit(-1);
         } else {
             //Using a delay to make sure the JavaScript is executed in the browser
             window.setTimeout(function () {
-                page.render("output.png");
                 phantom.exit();
-            }, 5000);
+            }, 10 * 1000);
         }
     });
-}
+
+}());
